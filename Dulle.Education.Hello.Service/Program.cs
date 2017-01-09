d1eb220e8b5870350be13d731379716983896b33
wim dedulle <wim_dedulle@tui.be>
2017-01-09 16:20:46 +0100

    first time commit

---
+++ Dulle.Education.Hello.Service/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dulle.Education.Hello.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddressHttp = "http://" 
+                + Environment.MachineName 
+                + ":80/Temporary_Listen_Addresses/helloservice";
+            //string baseAddressTcp = "net.tcp://" + Environment.MachineName + ":8731/Design_Time_Addresses/helloservice";
+
+            ServiceHost host = new ServiceHost(typeof(HelloService), new Uri(baseAddressHttp));
+            try
+            {
+                host.AddServiceEndpoint(
+                    typeof(IHello),
+                    new BasicHttpBinding(BasicHttpSecurityMode.None),
+                    "basic"
+                    );
+
+                host.Description.Behaviors.Add(new ServiceMetadataBehavior());
+                host.AddServiceEndpoint(
+                    typeof(IMetadataExchange),
+                    MetadataExchangeBindings.CreateMexHttpBinding(),
+                    "mex"
+                    );
+                host.Open();
+                PrintServiceInfo(host);
+                Console.ReadLine();
+                host.Close();
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                host.Abort();
+            }
+        }
+
+        static void PrintServiceInfo( ServiceHost host )
+        {
+            Console.WriteLine("{0} running", host.Description.ServiceType);
+            foreach( ServiceEndpoint endPoint in host.Description.Endpoints )
+            {
+                Console.WriteLine(">>> {0}", endPoint.Address);
+            }
+
+        }
+    }
+}
---
+++ Dulle.Education.Hello/HelloService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dulle.Education.Hello
+{
+    [DataContract]
+    public class Name
+    {
+        [DataMember]
+        public string Fist;
+        [DataMember]
+        public string Last;
+    }
+
+    [ServiceContract]
+    public interface IHello
+    {
+        [OperationContract]
+        string SayHello(Name person);
+    }
+
+    public class HelloService : IHello
+    {
+        public string SayHello(Name person)
+        {
+            return string.Format("Hello {0} {1}", person.Fist, person.Last);
+        }
+    }
+}
